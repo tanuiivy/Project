b9c5757f602662534fb10c1e06e5f306364e055c
tanuiivy <ivy.tanui@strathmore.edu>
2024-06-20 21:19:46 +0300

    adding php for inserting data

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<?php require_once("includes/db_connect.php"); ?>
+<?php require_once ("includes/db_connect.php") ; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,30 +9,61 @@
     <link rel="stylesheet" href="contact.css">
 </head>
 <body>
-    <?php include_once("template/nav.php"); ?>
+ 
+<?php include_once("template/nav.php");
+
+    if(isset($_POST["send_message"])){
+        $fn = $_POST["fullname"];
+        $mail= $_POST["email_address"];
+        $subject = $_POST["subject_line"];
+        $message = $_POST["text_message"];
     
-    <section id="contact">
-    <h2>CONTACT US</h2>
-        <p>We'd love to hear from you! Please reach us through the following contact details.</p>
-        <div class="contact-details">
-        <p><strong>Address:</strong> 123 Cafe Street, Coffee Town, CT 12345</p>
-        <p><strong>Phone:</strong> (254) 722-897462</p>
-        <p><strong>Email:</strong> contact@bnbcafe.com</p>
-    </div><br>
-    <p>You can also contact us by filling in the form below.</p>
-    <form action="" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br>
+    $insert_message = "INSERT INTO messages
+    (sender_name, sender_email, subject_line, text_message)
+    VALUES ('$fn' ,'$mail', '$subject', '$message')";
+    
+    if ($conn->query( $insert_message) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $insert_message . "<br>" . $conn->error;
+    }
+    }
+    
+    ?>
+   
+<section id="contact">
+<h2>CONTACT US</h2>
+<p>We'd love to hear from you! Please reach us through the following contact details.</p>
+<div class="contact-details">
+<p><strong>Address:</strong> 123 Cafe Street, Coffee Town, CT 12345</p>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
+<p><strong>Phone:</strong> (254) 722-897462</p>
 
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea><br>
+<p><strong>Email:</strong> contact@bnbcafe.com</p>
 
-        <button type="submit">Send Message</button>
-    </form>
+</div><br>
+
+<p>You can also contact us by filling in the form below.</p>
+
+<form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+<label for="fullname">Full Name:</label>
+<input type="text" id="fullname" name="fullname" required><br>
+
+    <label for="email_address">Email:</label>
+    <input type="email_address" id="email_address" name="email_address" required>
+
+    <label for="subject_line">Subject :</label>
+    <select name="subject_line" id="sl" name="subject_line" required>
+      <option value="">Select Subject</option>
+      <option value="Price Inquiries">Price Inquires </option>
+      <option value="Feedback">Feedback </option>
+    </select>  
+
+    <label for="text_message">Message:</label>
+    <textarea id="text_message" name="text_message" required></textarea><br>
+
+    <button type="submit">Send Message</button>
+</form>
 </section>
-<?php include_once("template/footer.php"); ?>
 </body>
 </html>
--- sql/dp.sql
+++ sql/dp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2024 at 02:54 PM
+-- Generation Time: Jun 20, 2024 at 04:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,19 +30,15 @@ USE `cafe`;
 -- Table structure for table `employees`
 --
 
-DROP TABLE IF EXISTS `employees`;
-CREATE TABLE IF NOT EXISTS `employees` (
-  `employeeID` bigint(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employees` (
+  `employeeID` bigint(10) NOT NULL,
   `userame` varchar(50) NOT NULL DEFAULT '',
   `email` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(50) DEFAULT NULL,
   `roleID` tinyint(1) NOT NULL DEFAULT 0,
   `gender` tinyint(1) NOT NULL DEFAULT 0,
   `datecreated` datetime DEFAULT current_timestamp(),
-  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`employeeID`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `roleID` (`roleID`)
+  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -51,15 +47,12 @@ CREATE TABLE IF NOT EXISTS `employees` (
 -- Table structure for table `gender`
 --
 
-DROP TABLE IF EXISTS `gender`;
-CREATE TABLE IF NOT EXISTS `gender` (
-  `genderID` tinyint(1) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `gender` (
+  `genderID` tinyint(1) NOT NULL,
   `gender` varchar(50) NOT NULL DEFAULT '',
   `datecreated` datetime DEFAULT current_timestamp(),
-  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`genderID`),
-  UNIQUE KEY `gender` (`gender`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `gender`
@@ -69,9 +62,71 @@ INSERT INTO `gender` (`genderID`, `gender`, `datecreated`, `dateupdated`) VALUES
 (1, 'Female', '2024-06-20 15:53:35', '2024-06-20 15:53:35'),
 (2, 'Male', '2024-06-20 15:53:35', '2024-06-20 15:53:35'),
 (3, 'Rather not say', '2024-06-20 15:53:35', '2024-06-20 15:53:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `messageID` bigint(10) NOT NULL,
+  `sender_name` varchar(50) NOT NULL DEFAULT '',
+  `sender_email` varchar(50) NOT NULL DEFAULT '',
+  `subject_line` text DEFAULT NULL,
+  `text_message` text DEFAULT NULL,
+  `datecreated` datetime DEFAULT current_timestamp(),
+  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employeeID`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `roleID` (`roleID`);
+
+--
+-- Indexes for table `gender`
+--
+ALTER TABLE `gender`
+  ADD PRIMARY KEY (`genderID`),
+  ADD UNIQUE KEY `gender` (`gender`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`messageID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employeeID` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `gender`
+--
+ALTER TABLE `gender`
+  MODIFY `genderID` tinyint(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `messageID` bigint(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
